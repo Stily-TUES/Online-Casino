2abf3b482ef3fc01cb8cc608381b0c79b036bc81
Stily-TUES <stilianqo@gmail.com>
2024-01-14 19:29:37 +0200

    minor fix to dependency

--- online-casino-backend/src/main/java/com/example/onlinecasinobackend/gameLogic/SlotMachine.java
+++ online-casino-backend/src/main/java/com/example/onlinecasinobackend/gameLogic/SlotMachine.java
@@ -11,6 +11,9 @@ public class SlotMachine {
         this.symbols = symbols;
         this.paylines = paylines;
     }
+    public double getJackpotPool() {
+        return jackpotPool;
+    }
 
     public void spin(User user) {
         // Check if the user has enough balance
@@ -18,19 +21,16 @@ public class SlotMachine {
             System.out.println("Insufficient funds. Please add more money to your balance.");
             return;
         }
-    
-        // Deduct the bet amount from the user's balance
+
         user.deductBalance(BET_AMOUNT);
     
-        // Add $1 to the jackpot pool for every spin
+
         jackpotPool += BET_AMOUNT;
     
-        // Spin each reel
         for (Reel reel : reels) {
             reel.spin();
         }
     
-        // Check combinations on paylines and calculate winnings
         for (Payline payline : paylines) {
             Map.Entry<Integer, Symbol> winningInfo = getWinningCountAndSymbol(payline);
             int winningCount = winningInfo.getKey();
@@ -41,7 +41,6 @@ public class SlotMachine {
                     double jackpotWinnings = jackpotPool;
                     user.addBalance(jackpotWinnings);
                     System.out.println("Jackpot! You won $" + jackpotWinnings + "!");
-                    // Reset the jackpot pool after a jackpot win
                     jackpotPool = 0.0;
                 } else {
                     double regularWinnings = calculateWinnings(winningSymbol, winningCount);
