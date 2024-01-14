78190da8c8641936e5b21544579d0840a570d2ad
Stily-TUES <stilianqo@gmail.com>
2024-01-14 21:01:18 +0200

    Bug fixing

---
+++ online-casino-backend/src/main/java/com/example/onlinecasinobackend/dto/UserRegistrationDTO.java
@@ -0,0 +1,2 @@
+package com.example.onlinecasinobackend.dto;public class UserRegistrationDTO {
+}
---
+++ online-casino-backend/src/main/java/com/example/onlinecasinobackend/service/UserDetailsService.java
@@ -0,0 +1,2 @@
+package com.example.onlinecasinobackend.service;public interface UserDetailsService {
+}
