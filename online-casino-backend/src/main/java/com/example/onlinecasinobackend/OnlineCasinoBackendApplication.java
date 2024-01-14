75ff4aee9c8ef806af70bf0af0fc67c161311fe4
Stily-TUES <stilianqo@gmail.com>
2024-01-14 12:26:20 +0200

    first commit

---
+++ online-casino-backend/src/main/java/com/example/onlinecasinobackend/OnlineCasinoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinecasinobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCasinoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCasinoBackendApplication.class, args);
+	}
+
+}
---
+++ online-casino-backend/src/test/java/com/example/onlinecasinobackend/OnlineCasinoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinecasinobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCasinoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
